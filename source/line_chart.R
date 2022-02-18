4a9ddd2a289d2a499f0912b7e84550d04818fc67
virurep <virurep@uw.edu>
2022-02-18 01:20:01 -0800

    summary plus chart 2

---
+++ source/line_chart.R
@@ -0,0 +1,18 @@
+nyc_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-aWhale7/main/data/SNAP_data.csv", stringsAsFactors = FALSE)
+library(tidyverse)
+library(ggplot2)
+
+total_per_year <- nyc_data %>% 
+  group_by(Year) %>% 
+  summarize(sum(Total.SNAP.Households)) 
+
+names(total_per_year) [2] <- "households"
+
+ggplot(data = total_per_year, aes(x = Year, y = households, group = 1)) +
+  geom_line(
+    color = "purple"
+  )+
+  geom_point()+
+  labs(
+    title = "Total SNAP Households Per Year",
+  )
