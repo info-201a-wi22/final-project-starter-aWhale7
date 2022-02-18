7d0f44ef0433951bb32509984f65daeb0a80d2e1
MatthewKylePedraja <mpedraj@uw.edu>
2022-02-18 01:19:50 -0800

    Area Chart: New York City, New York

--- source/AreaChart.R
+++ source/AreaChart.R
@@ -11,17 +11,20 @@ snap_benefits <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/fina
 most_benefits <- snap_benefits %>%
   # pipe operator to access the `snap_benefits` variable created on top
   group_by(Year, Month) %>%
+  filter(Year >= 2015 & Year <= 2021) %>%
   # groups by year and month
   filter(Total.SNAP.Benefits == max(Total.SNAP.Benefits)) %>%
   # filter to only get the highest amount of total snap benefits in each month in each year
-  select(Year, Month, Total.SNAP.Benefits)
+  select(Year, Month, Total.SNAP.Benefits, District)
   # selects the Year, Month, and Total Snap Benefit to be included in the data frame
 
 # pipe operator to access most_benefits data frame
 most_benefits %>%
   # area chart
-  ggplot(aes(x = Year, y = Total.SNAP.Benefits, fill = Month)) +
-         geom_area(alpha = 1000, size = 1, color = "black")
+  ggplot(aes(x = Year, y = Total.SNAP.Benefits, fill = Month))+
+         geom_area(alpha = 10, size = 1.0, color = "black")+
+         ggtitle("Snap Benefits in New York City, New York")+
+         labs(y = "Total Snap Benefits (in $)", x = "Year (2015-2021)")
   # x-axis is the Year, y is the highest Total Snap Benefits in each month and each year, fill is the month
-  # 
+
   
