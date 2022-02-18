5499f445ff9c9922e1b9e1f8d21d8d1b4e3ca0da
MatthewKylePedraja <mpedraj@uw.edu>
2022-02-18 01:49:10 -0800

    Area Chart: New York City, New York R Code

--- source/AreaChart.R
+++ source/AreaChart.R
@@ -7,24 +7,26 @@ library("ggplot2")
 # load CSV file onto variable `snap_benefits`
 snap_benefits <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-aWhale7/main/data/SNAP_data.csv")
 
-# title variable `most_benefits` and create a data frame with Year, Month, and total snap benefits
-most_benefits <- snap_benefits %>%
-  # pipe operator to access the `snap_benefits` variable created on top
-  group_by(Year, Month) %>%
+# title variable `New_York_benefits` and create a data frame with Year, Month, and total snap benefits
+New_York_benefits <- snap_benefits %>%
+  # pipe operator to access the `snap_benefits` variable created 
+  group_by(Year, Month, District) %>%
+  # groups the data frame by Year, Month, and District
+  filter(District == "New York City")%>%
+  # filters the district variable to only show New York City, the district that will be focused on
   filter(Year >= 2015 & Year <= 2021) %>%
-  # groups by year and month
-  filter(Total.SNAP.Benefits == max(Total.SNAP.Benefits)) %>%
-  # filter to only get the highest amount of total snap benefits in each month in each year
+  # filters the Year variable to only include 2015 to 2021
   select(Year, Month, Total.SNAP.Benefits, District)
-  # selects the Year, Month, and Total Snap Benefit to be included in the data frame
+  # selects the Year, Month, and Total Snap Benefit to be included in the `most_benefits` data frame
 
-# pipe operator to access most_benefits data frame
-most_benefits %>%
+New_York_benefits %>%
   # area chart
   ggplot(aes(x = Year, y = Total.SNAP.Benefits, fill = Month))+
+  # x-axis is the Year, y is the highest Total Snap Benefits in each month and each year, with color filled month
          geom_area(alpha = 10, size = 1.0, color = "black")+
          ggtitle("Snap Benefits in New York City, New York")+
+  # title for the Area Chart
          labs(y = "Total Snap Benefits (in $)", x = "Year (2015-2021)")
-  # x-axis is the Year, y is the highest Total Snap Benefits in each month and each year, fill is the month
+  # labels for x and y axis
 
   
